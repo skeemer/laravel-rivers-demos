3aa911d7464a9b3a2bf823244dd1888e12104bd1
Leo Lutz <leo@leolutz.com>
2026-02-12 11:32:47 -0700

    Make vite config friendly to non-ddev and auto-run in vite

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,28 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
+const ddev = ! process.env.DDEV_PRIMARY_URL_WITHOUT_PORT ? {} : {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    origin: `${process.env.DDEV_PRIMARY_URL_WITHOUT_PORT}:5173`,
+    cors: {
+        origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+    },
+}
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/app-joint.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
-        host: '0.0.0.0',
-        cors: process.env.DDEV_HOSTNAME,
-        hmr: {
-            host: process.env.DDEV_HOSTNAME,
-            protocol: 'wss',
-        },
-    }
+        ...ddev,
+    },
 });
